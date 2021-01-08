f4d3899896baf85531628f84bab393d4bd033aa3
Kristian Koehntopp <kris@koehntopp.de>
2021-01-08 20:02:31 +0100

    Functional version?

--- src/backtracker.py
+++ src/backtracker.py
@@ -1,43 +1,93 @@
 #! /usr/bin/env python3
 
-from typing import List, Optional
+from typing import List, Dict, Optional
+from random import shuffle
+
+
+def safe_list_get(l, idx, default):
+    try:
+        return l[idx]
+    except IndexError:
+        return default
+
 
 class Labyrinth:
     width: int = 10
     height: int = 0
     l: List[List[int]] = None
-    dir: List[List[int]] = None
 
-    def __str__(self):
-        s= ""
+    directions: Dict[str, int] = {"N": 1, "S": 2, "E": 4, "W": 8}
+    opposite: Dict[str, str] = {"N": "S", "S": "N", "W": "E", "E": "W"}
 
-        for i in self.l:
-            for j in i:
-                 s += f"{j} "
+    dx: Dict[str, int] = {"N": 0, "S": 0, "E": 1, "W": -1}
+    dy: Dict[str, int] = {"N": -1, "S": 1, "E": 0, "W": 0}
+
+    def __str__(self) -> str:
+
+        # Als Zahlen drucken
+        s = ""
+        for y in range(0, self.height):
+            s += f"{y=} "
+            for x in range(0, self.width):
+                s += f"{self.l[y][x]} "
             s += "\n"
+        s += "\n"
 
+        # Als Labyrinth drucken
+        # Zeilen
+        s += "--" + "-" * (self.width * 2 + 1) + "\n"
+        for y in range(0, self.height):
+            s += f"{y}|"
+
+            for x in range(0, self.width):
+                el = self.l[y][x]  # aktuelles Feld auslesen
+
+                # Haben wir einen Durchgang nach Süden offen? Dann " "
+                s += " " if el & self.directions["S"] else "-"
+
+                # Haben wir einen Durchgang nach Osten offen (oder von Osten kommend?)
+                if el & self.directions["E"]:
+                    check = el | self.l[y][x+1]
+                    s += " " if check & self.directions["S"] else "-"
+                else:
+                    s += "|"
+            s += "\n"
+        s += "--" + "-" * (self.width * 2 + 1) + "\n"
         return s
 
     def __init__(self, width=10, height=10):
+        # Merken der Labyrinthgroesse
         self.width = width
         self.height = height
 
-        self.l = [[0] * width] *height
-
-        self.dir = {
-            "N": {"x": 0, "y": -1 },
-            "S": {"x": 0, "y": +1},
-            "E": {"x": -1, "y": 0},
-            "W": {"x": +1, "y": -},
-        }
+        # leeres Labyrinth
+        self.l = [[]] * self.height
+        for y in range(0, self.height):
+            self.l[y] = [0] * self.width
 
         return
 
-    def carve_from(self, x=0, y=0):
-        directions = ["N", "E", "S", "W"]
+    def carve_from(self, x=0, y=0) -> None:
+        # Mögliche Schritte mischen
+        directions = list(self.directions.keys())
         shuffle(directions)
 
+        # Rundum durchprobieren
         for d in directions:
+            # neues Feld berechnen
+            nx, ny = x + self.dx[d], y + self.dy[d]
+
+            # Wenn das neue Feld gültige Koordinaten hat:
+            if 0 <= nx < self.width and 0 <= ny < self.height:
+                # Wenn das neue Feld leer ist:
+                if self.l[ny][nx] == 0:
+                    # Altes Feld: Mauer in die neue Richtung einreissen
+                    self.l[y][x] |= self.directions[d]
+                    # Neues Feld: Mauer aus der neuen Richtung einreissen
+                    self.l[ny][nx] |= self.directions[self.opposite[d]]
+
+                    # Weiter marschieren
+                    self.carve_from(nx, ny)
 
 
 if __name__ == "__main__":
