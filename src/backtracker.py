5c698e9e251a0cfe60403da9d1a7b738b6c059ab
Kristian Koehntopp <kris@koehntopp.de>
2021-01-09 10:41:12 +0100

    Add turtle graphics

--- src/backtracker.py
+++ src/backtracker.py
@@ -2,6 +2,8 @@
 
 from typing import List, Dict
 from random import shuffle
+import turtle
+from time import sleep
 
 
 class Labyrinth:
@@ -9,7 +11,7 @@ class Labyrinth:
     height: int
     grid: List[List[int]]
 
-    directions: Dict[str, int] = {"N": 1, "S": 2, "E": 4, "W": 8}
+    directions: Dict[str, int] = {"N": 1, "E": 2, "S": 4, "W": 8}
     opposite: Dict[str, str] = {"N": "S", "S": "N", "W": "E", "E": "W"}
 
     dx: Dict[str, int] = {"N": 0, "S": 0, "E": 1, "W": -1}
@@ -18,35 +20,14 @@ class Labyrinth:
     def __str__(self) -> str:
         s = ""
 
-        # # Als Zahlen drucken
-        # for y in range(0, self.height):
-        #     s += f"{y=} "
-        #     for x in range(0, self.width):
-        #         s += f"{self.grid[y][x]} "
-        #     s += "\n"
-        # s += "\n"
-
-        # Als Labyrinth drucken
-        # Zeilen
-        s += "--" + "-" * (self.width * 2 + 1) + "\n"
+        # Als Zahlen drucken
         for y in range(0, self.height):
-            s += f"{y}|"
-
-            # Spalten
+            s += f"{y=} "
             for x in range(0, self.width):
-                el = self.grid[y][x]  # aktuelles Feld auslesen
-
-                # Haben wir einen Durchgang nach Süden offen? Dann " "
-                s += " " if el & self.directions["S"] else "-"
-
-                # Haben wir einen Durchgang nach Osten offen (oder von Osten kommend?)
-                if el & self.directions["E"]:
-                    check = el | self.grid[y][x + 1]
-                    s += " " if check & self.directions["S"] else "-"
-                else:
-                    s += "|"
+                s += f"{self.grid[y][x]} "
             s += "\n"
-        s += "--" + "-" * (self.width * 2 + 1) + "\n"
+        s += "\n"
+
         return s
 
     def __init__(self, width: int = 10, height: int = 10) -> None:
@@ -87,8 +68,49 @@ class Labyrinth:
                     # Weiter marschieren
                     self.carve_from(nx, ny)
 
+    def show_init(self) -> turtle.Turtle:
+        s = turtle.getscreen()
+        t = turtle.Turtle()
+        t.penup()
+        t.goto(-150, 150)
+        t.pendown()
+
+        return t
+
+    def square(self, t: turtle.Turtle, corners: int) -> None:
+        t.speed(0)
+        t.seth(0)
+
+        t.pendown()
+        for i in range(0,4):
+            p = 2**i
+            t.pencolor("black")
+            if corners & p:
+                t.pencolor("white")
+
+            t.forward(30)
+            t.right(90)
+
+        t.penup()
+        t.forward(30)
+
+    def show(self):
+        t = self.show_init()
+
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                el = self.grid[y][x]
+                self.square(t, el)
+            t.seth(180)
+            t.forward(30 * self.width)
+            t.seth(270)
+            t.forward(30)
+
+        sleep(10)
+
 
 if __name__ == "__main__":
     grid = Labyrinth()
     grid.carve_from()
     print(grid)
+    grid.show()
