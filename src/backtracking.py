a1703649d93d9dc95dd1843f859490c760ffa2ab
Kristian Koehntopp <kris@koehntopp.de>
2021-01-12 21:05:47 +0100

    Refactor carve_more

--- src/backtracking.py
+++ src/backtracking.py
@@ -34,29 +34,3 @@ class Backtracking(Labyrinth):
             if self[np] == 0:
                 self.make_passage(pos, d)
                 self.carve(np, show=show)
-
-    def carve_more(self, walls_to_remove: int = 0, show: Any = None):
-        """Remove walls_to_remove more walls, randomly.
-
-        After applying carve(), we randomly delete walls_to_remove many more walls
-        to create for multiple valid pathes to the goal.
-
-        We guarantee that each new passage is actually a wall removed, and that
-        new passages do not lead off the grid.
-        """
-        to_go = walls_to_remove
-        while to_go:
-            # Choose a random position and direction
-            pos = Pos((randrange(self.width), randrange(self.height)))
-            d = self.random_directions()[0]
-
-            # and check if deleting the wall would change things.
-            if self.can_make_passage(pos, d):
-                new_pos = self.step(pos, d)
-
-                if show:
-                    show(self, red=pos, green=new_pos)
-
-                # guaranteed to stay on grid and change the world
-                self.make_passage(pos, d)
-                to_go -= 1  # one done
